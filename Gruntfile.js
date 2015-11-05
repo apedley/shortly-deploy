ec1e26b6d5f8220d19971b451c8e0d467c069906
apedley <apedley@gmail.com>
2015-11-04 18:34:55 -0600

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,7 +124,6 @@ module.exports = function(grunt) {
         'mochaTest'
     ]);
 
-
     grunt.registerTask('upload', function(n) {
         if (grunt.option('prod')) {
             // add your production server task here
